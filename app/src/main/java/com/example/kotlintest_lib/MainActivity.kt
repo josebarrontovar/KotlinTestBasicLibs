40cd92ad0781ddc644ce86be3349c48850debacb
josebarrontovar <josebarrontovar@hotmail.com>
2025-04-21 16:32:42 -0600

    test run ci/cd/

--- app/src/main/java/com/example/kotlintest_lib/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest_lib/MainActivity.kt
@@ -10,6 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
     }
 }
